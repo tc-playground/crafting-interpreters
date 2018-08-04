d9c058284f2bd29c171f4603c079880f92c879f6
Tim Langford <tim.langford@gmail.com>
2018-08-04 17:33:53 +0100

    Added visitor pattern to types.

--- lox-interpreter/src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ lox-interpreter/src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -34,11 +34,18 @@ public class GenerateAst {
         writer.println("");
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
+        // The AST classes.
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim(); 
             defineType(writer, baseName, className, fields);
         }
+
+            // The base accept() method.
+        writer.println("");
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
@@ -61,6 +68,13 @@ public class GenerateAst {
 
         writer.println("    }");
 
+        // Visitor pattern.
+        writer.println();
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+            className + baseName + "(this);");
+        writer.println("    }");
+
         // Fields.
         writer.println();
         for (String field : fields) {
@@ -68,6 +82,20 @@ public class GenerateAst {
         }
 
         writer.println("  }");
+    }
+
+    // Generate the Vistor Interface
+    private static void defineVisitor(
+        PrintWriter writer, String baseName, List<String> types) {
+      writer.println("  interface Visitor<R> {");
+  
+      for (String type : types) {
+        String typeName = type.split(":")[0].trim();
+        writer.println("    R visit" + typeName + baseName + "(" +
+            typeName + " " + baseName.toLowerCase() + ");");
+      }
+  
+      writer.println("  }");
     }
 
 }
