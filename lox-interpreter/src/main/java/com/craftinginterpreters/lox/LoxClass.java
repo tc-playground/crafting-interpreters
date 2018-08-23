ffd4fd2f2934203734ec75d9c4a3cf9c326fcc75
Tim Langford <tim.langford@gmail.com>
2018-08-23 22:14:12 +0100

    Implements basic instance.

--- lox-interpreter/src/main/java/com/craftinginterpreters/lox/LoxClass.java
+++ lox-interpreter/src/main/java/com/craftinginterpreters/lox/LoxClass.java
@@ -3,11 +3,22 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 import java.util.Map;
 
-class LoxClass {
+class LoxClass implements LoxCallable {
     final String name;
 
     LoxClass(String name) {
         this.name = name;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+  
+    @Override
+    public int arity() {
+        return 0;
     }
 
     @Override
